d6e4f5758e12cbb4afe816d88461b13e87dd8c0d
unknown <wkdgusqhd080@naver.com>
2020-06-18 00:04:19 +0900

    사칙연산 테스트

--- src/main/java/com/example/test/controller/IndexController.java
+++ src/main/java/com/example/test/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.example.test.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,37 +14,42 @@ import com.example.test.service.OperationService;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@RestController
+@Controller
+@RequestMapping("index")
 public class IndexController {
 	
 	@Autowired
 	OperationService operationService;
 	
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public @ResponseBody String index() {
-		return "Hello World";
+	@RequestMapping(value = "hello", method = RequestMethod.GET)
+	public String hello(Model model) {
+		model.addAttribute("hello","Hello World");
+		return "hello";
 	}
 	
-	@RequestMapping(value = "/plus", method = RequestMethod.GET)
-	public @ResponseBody String add(@RequestParam int value1, @RequestParam int value2) {
-		return value1 + " + " + value2 + " = " + operationService.plusOperation(value1, value2);
+	@RequestMapping(value = "plus", method = RequestMethod.GET)
+	public String plus(Model model, @RequestParam int value1, @RequestParam int value2) {
+		model.addAttribute("operation", value1 + " + " + value2 + " = " + operationService.plusOperation(value1, value2));
+		return "operation";
 	}
 	
-	@RequestMapping(value = "/minus", method = RequestMethod.GET)
-	public @ResponseBody String minus(@RequestParam int value1, @RequestParam int value2) {
-		return value1 + " - " + value2 + " = " + operationService.minusOperation(value1, value2);
-		
+	@RequestMapping(value = "minus", method = RequestMethod.GET)
+	public String minus(Model model, @RequestParam int value1, @RequestParam int value2) {
+		model.addAttribute("operation", value1 + " - " + value2 + " = " + operationService.minusOperation(value1, value2));
+		return "operation";
 	}
 	
-	@RequestMapping(value = "/division", method = RequestMethod.GET)
-	public @ResponseBody String division(@RequestParam int value1, @RequestParam int value2) {
-		return value1 + " / " + value2 + " = " + operationService.divisionOperation(value1, value2);
+	@RequestMapping(value = "division", method = RequestMethod.GET)
+	public String division(Model model, @RequestParam int value1, @RequestParam int value2) {
+		model.addAttribute("operation", value1 + " / " + value2 + " = " + operationService.divisionOperation(value1, value2));
+		return "operation"; 
 	}
 	
-	@RequestMapping(value = "/multiply", method = RequestMethod.GET)
-	public @ResponseBody String multiply(@RequestParam int value1, @RequestParam int value2) {
-		return value1 + " * " + value2 + " = " + operationService.multiplyOperation(value1, value2);
+	@RequestMapping(value = "multiply", method = RequestMethod.GET)
+	public String multiply(Model model, @RequestParam int value1, @RequestParam int value2) {
+		model.addAttribute("operation", value1 + " * " + value2 + " = " + operationService.multiplyOperation(value1, value2));
+		return "operation";
+			
 	}
-	
 	
 }
